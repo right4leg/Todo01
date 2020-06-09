d253a8eb5689fe9d19fd381cbf504aef670a753a
right4leg <right4leg0727@icloud.com>
2020-06-09 22:00:47 +0900

    TODO(TableView勉強)

--- Todo01/ViewController.swift
+++ Todo01/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let TODO = ["にんじん","玉ねぎ","じゃがいも"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = TODO[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    }
 
 
 }
